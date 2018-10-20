0c7a5bc3b6d7ad6a21f2debcdfd4039debd341c8
Intija <Intija>
2018-10-20 11:08:42 -0700

    Control video states from main app thread

---
+++ intijavid/src/main/java/com/intija/intijavid/VideoStateController.kt
@@ -0,0 +1,12 @@
+package com.intija.intijavid
+
+/**
+ * Created by Intija on 10/8/2018.
+ */
+interface VideoStateController {
+
+    fun pauseVideo()    fun playVideo()
+
+    fun isPlaying(): Boolean
+
+}
